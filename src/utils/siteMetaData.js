4a1cc7d1e8ff3ad13523ecc712c604046c7646af
HelenaLannes18 <helena.lannes.salles@hotmail.com>
2025-03-20 11:37:08 -0300

    enviando alteracoes no mdx

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,22 +1,22 @@
 // please update required information
 const siteMetadata = {
-  title: 'Next.js Blog With Tailwind CSS and Contentlayer',
-  author: 'Helena Lannes',
-  headerTitle: 'Next.js Blog',
-  description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
-  language: 'en-us',
+  title: 'Blog Disciplina',
+  author: 'Fisiocomp',
+  headerTitle: 'Blog',
+  description: 'Blog para a disciplina.',
+  language: 'pt-br',
   theme: 'system', // system, dark or light
   siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
-  siteLogo: '/logo.png',
+  siteLogo: '/next.svg',
   socialBanner: '/social-banner.png', // add social banner in the public folder
   email: 'Helena27@gmail.com',
   github: 'https://github.com/Helena27',
   twitter: 'https://twitter.com/code_bucks',
   facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com/Helena',
-  linkedin: 'https://www.linkedin.com/in/Helena/',
+  youtube: 'https://youtube.com/Fisiocomp',
+  linkedin: 'https://www.linkedin.com/in/Fisiocomp/',
   dribbble: 'https://www.dribbble.com',
-  locale: 'en-US',
+  locale: 'pt-br',
 };
 
 module.exports = siteMetadata;

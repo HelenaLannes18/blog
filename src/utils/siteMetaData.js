bca9fcc3111e5aa9382eb09408df4f09c6f7ec48
HelenaLannes18 <helena.lannes.salles@hotmail.com>
2025-03-20 11:12:08 -0300

    enviando alteracoes

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+  title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+  author: 'Helena Lannes',
+  headerTitle: 'Next.js Blog',
+  description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+  siteLogo: '/logo.png',
+  socialBanner: '/social-banner.png', // add social banner in the public folder
+  email: 'Helena27@gmail.com',
+  github: 'https://github.com/Helena27',
+  twitter: 'https://twitter.com/code_bucks',
+  facebook: 'https://facebook.com',
+  youtube: 'https://youtube.com/Helena',
+  linkedin: 'https://www.linkedin.com/in/Helena/',
+  dribbble: 'https://www.dribbble.com',
+  locale: 'en-US',
+};
+
+module.exports = siteMetadata;
